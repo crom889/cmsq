55ea431725a0a49a572c8d8bb8377f027c4a9e96
crom889 <114574647+crom889@users.noreply.github.com>
2022-09-28 17:51:07 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,23 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('https://api.air0l.cc', "_blank");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('https://api.air3l.cc', "_blank");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('https://api.air0l.cc', "_blank");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('https://api.air3l.cc', "_blank");
+        }
+        document.getElementById("btnEnter5").onclick = function() {
+            window.open('https://api.air0l.cc', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);
