f0db4d6329481cbafe26cd5bc86949b6ca4dfb36
crom889 <114574647+crom889@users.noreply.github.com>
2022-12-02 16:23:44 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://api.87k9vwzr.club', "_blank");
+            window.open('https://api.uqexfka7.club', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://api.lmd60yd0.cc', "_blank");
+            window.open('https://api.bv34u2ny.world', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://api.87k9vwzr.club', "_blank");
+            window.open('https://api.uqexfka7.club', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://api.lmd60yd0.cc', "_blank");
+            window.open('https://api.bv34u2ny.world', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('https://api.87k9vwzr.club', "_blank");
+            window.open('https://api.uqexfka7.club', "_blank");
         }
     }
     function init() {
